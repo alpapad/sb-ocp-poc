df4ae1e73780f02d6ce8d3f7b401c4274dde2245
Alexandros Papadakis <alpapad@gmail.com>
2022-09-28 12:05:37 +0300

    Adds opentracing (via zipkin/brave) to grpc client & server.

--- spring-grpc-server/src/main/java/com/apapadak/poc/service/EmployeeService.java
+++ spring-grpc-server/src/main/java/com/apapadak/poc/service/EmployeeService.java
@@ -4,6 +4,9 @@ import com.techgeeknext.constants.Role;
 
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.apapadak.poc.grpc.EmployeeRequest;
 import com.apapadak.poc.grpc.EmployeeResponse;
 import com.apapadak.poc.grpc.EmployeeServiceGrpc;
@@ -12,7 +15,7 @@ import net.devh.boot.grpc.server.service.GrpcService;
 
 @GrpcService
 public class EmployeeService extends EmployeeServiceGrpc.EmployeeServiceImplBase {
-
+    private static Logger logger = LoggerFactory.getLogger(EmployeeService.class);
     /**
      * Unary operation to get the employee based on employee id
      * @param request
@@ -22,6 +25,7 @@ public class EmployeeService extends EmployeeServiceGrpc.EmployeeServiceImplBase
     public void getEmployee(EmployeeRequest request,
                             StreamObserver<EmployeeResponse> responseObserver) {
 
+        logger.info("Got request for empid: {}", request.getEmpId());
         //call repository to load the data from database
         //we have added static data for example
         EmployeeResponse empResp = EmployeeResponse
@@ -36,5 +40,6 @@ public class EmployeeService extends EmployeeServiceGrpc.EmployeeServiceImplBase
 
         //mark process is completed
         responseObserver.onCompleted();
+        
     }
 }
